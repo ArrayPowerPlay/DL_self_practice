a89493dafadcec437360e01b91b92bdb347f8208
ArrayPowerPlay <giadinhtranspy@gmai.com>
2026-02-06 15:23:46 +0700

    s

--- utils/spy.py
+++ utils/spy.py
@@ -1249,7 +1249,8 @@ def download(url, folder='../data', sha1_hash=None):
             return fname
     # Download if cache miss
     print(f"Downloading {fname} from {url}...")
-    r = requests.get(url, stream=True, verify=True)
+    r = requests.get(url, stream=True)
+    r.raise_for_status()
     with open(fname, 'wb') as f:        # wb = write binary
         f.write(r.content)
     return fname
@@ -1565,3 +1566,234 @@ def plot(X, Y=None, xlabel=None, ylabel=None, legend=[], xlim=None, ylim=None,
     for x, y, fmt in zip(X, Y, fmts):
         axes.plot(x, y, fmt) if len(x) else axes.plot(y, fmt)
     set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+
+def get_tokens_and_segments(tokens_a, tokens_b=None):
+    """"Get tokens of the BERT input sequence and their segment ID"""
+    tokens = ['<cls>'] + tokens_a + ['<sep>']
+    # 0 and 1 are marking segment A and B, respectively
+    segments = [0] * (len(tokens_a) + 2)
+    if tokens_b is not None:
+        tokens += tokens_b + ['<sep>']
+        segments += [1] * (len(tokens_b) + 1)
+    return tokens, segments
+
+
+class BERTEncoder(nn.Module):
+    """BERT Encoder"""
+    def __init__(self, vocab_size, num_hiddens, ffn_num_hiddens, num_heads,
+                 num_blks, dropout, max_len=1000, **kwargs):
+        super().__init__(**kwargs)
+        self.token_embedding = nn.Embedding(vocab_size, num_hiddens)
+        self.segment_embedding = nn.Embedding(2, num_hiddens)
+        # In BERT, positional embedding are learnable
+        self.pos_embedding = nn.Parameter(torch.randn(1, max_len, num_hiddens))
+        self.blks = nn.Sequential()
+        for i in range(num_blks):
+            self.blks.add_module(f"{i}", TransformerEncoderBlock(
+                num_hiddens, ffn_num_hiddens, num_heads, dropout, True
+            ))
+
+    def forward(self, tokens, segments, valid_lens):
+        X = self.token_embedding(tokens) + self.segment_embedding(segments)
+        X = X + self.pos_embedding[:, :X.shape[1], :]
+        for blk in self.blks:
+            X = blk(X, valid_lens)
+        return X
+    
+
+class MaskLM(nn.Module):
+    """Masked Language Model implement for BERT"""
+    def __init__(self, vocab_size, num_hiddens, **kwargs):
+        super().__init__(**kwargs)
+        self.mlp = nn.Sequential(
+            nn.LazyLinear(num_hiddens),
+            nn.ReLU(),
+            nn.LayerNorm(num_hiddens),
+            nn.LazyLinear(vocab_size)
+        )
+
+
+    def forward(self, X, pred_positions):
+        # pred_positions shape: (batch_size, num_pred_positions)
+        num_pred_positions = pred_positions.shape[1]
+        pred_positions = pred_positions.reshape(-1)
+        batch_size = X.shape[0]
+        batch_idx = torch.arange(0, batch_size)
+        # Suppose that `batch_size` = 2, `num_pred_positions` = 3, then
+        # `batch_idx` is `torch.tensor([0, 0, 0, 1, 1, 1])`
+        batch_idx = torch.repeat_interleave(batch_idx, num_pred_positions)
+        # masked_X shape: (batch_size * num_pred_positions, num_hiddens)
+        masked_X = X[batch_idx, pred_positions]
+        masked_X = masked_X.reshape((batch_size, num_pred_positions, -1))
+        mlm_Y_hat = self.mlp(masked_X)
+        return mlm_Y_hat
+    
+
+class NextSequencePred(nn.Module):
+    """The next sequence prediction task of BERT"""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.output = nn.LazyLinear(2)
+
+    
+    def forward(self, X):
+        # X size: (batch_size, num_hiddens) -> X is <cls> token
+        return self.output(X)
+    
+
+def _read_wiki(data_dir):
+    file_name = os.path.join(data_dir, 'wiki.train.tokens')
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    # Uppercase letters are converted to lowercase
+    paragraphs = [lines.strip().lower().split(' . ') 
+                  for line in lines if len(line.split(' . ')) >= 2]
+    random.shuffle(paragraphs)
+    # paragraphs is a list of list of lists
+    return paragraphs
+
+
+def _get_next_sentence(sentence, next_sentence, paragraphs):
+    if random.random() < 0.5:
+        is_next = True
+    else:
+        next_sentence = random.choice(random.choice(paragraphs))
+        is_next = False
+    return sentence, next_sentence, is_next
+
+
+def _get_nsp_data_from_paragraph(paragraph, paragraphs, max_len):
+    nsp_data_from_paragraph = []
+    for i in range(len(paragraph) - 1):
+        tokens_a, tokens_b, is_next = _get_next_sentence(
+            paragraph[i], paragraph[i + 1], paragraphs
+        )
+        # 2 <sep> tokens and 1 <cls> token
+        if len(tokens_a) + len(tokens_b) + 3 > max_len:
+            continue
+        tokens, segments = spy.get_tokens_and_segments(tokens_a, tokens_b)
+        nsp_data_from_paragraph.append((tokens, segments, is_next))
+    return nsp_data_from_paragraph
+
+
+def _replace_mlm_tokens(tokens, candidate_pred_positions, num_mlm_preds, vocab):
+    mlm_input_tokens = [token for token in tokens]
+    pred_positions_and_labels = []
+    random.shuffle(candidate_pred_positions)
+
+    for mlm_pred_position in candidate_pred_positions:
+        if len(pred_positions_and_labels) >= num_mlm_preds:
+            break
+        masked_token = None
+        # 80% of the time -> replace the word with the '<mask>' token
+        if random.random() < 0.8:
+            masked_token = '<mask>'
+        else:
+            # 10% of the time -> keep the word unchanged
+            if random.random() < 0.5:
+                masked_token = tokens[mlm_pred_position]
+            # 10% of the time -> replace the word with a random word
+            else:
+                masked_token = random.choice(vocab.idx_to_token)
+        mlm_input_tokens[mlm_pred_position] = masked_token
+        pred_positions_and_labels.append((mlm_pred_position, tokens[mlm_pred_position]))
+    
+    return mlm_input_tokens, pred_positions_and_labels
+
+
+def _get_mlm_data_from_tokens(tokens, vocab):
+    candidate_pred_positions = []
+    # 'tokens' is a list of string
+    for i, token in enumerate(tokens):
+        if token in ['<cls>', '<sep>']:
+            continue
+        candidate_pred_positions.append(i)
+    # 15% of random tokens are predicted in the masked language modeling task
+    mlm_num_preds = max(1, round(len(tokens) * .15))
+    mlm_input_tokens, pred_positions_and_labels = _replace_mlm_tokens(
+        tokens, candidate_pred_positions, mlm_num_preds, vocab)
+    pred_positions_and_labels = sorted(pred_positions_and_labels, key=lambda x: x[0])
+    pred_positions = [v[0] for v in pred_positions_and_labels]
+    mlm_pred_labels = [v[1] for v in pred_positions_and_labels]
+
+    return vocab[mlm_input_tokens], pred_positions, vocab[mlm_pred_labels]
+
+
+def _pad_BERT_inputs(examples, max_len, vocab):
+    max_num_mlm_preds = round(max_len * .15)
+    all_token_ids, all_segments, valid_lens = [], [], []
+    all_pred_positions, all_mlm_weights, all_mlm_labels = [], [], []
+    nsp_labels = []
+    
+    for (
+        token_ids, 
+        pred_positions, 
+        mlm_pred_label_ids, 
+        segments, 
+        is_next
+    ) in examples:
+        all_token_ids.append(torch.tensor(
+            token_ids + [vocab['<pad>']] * (max_len - len(token_ids)), 
+            dtype=torch.long))
+        all_segments.append(torch.tensor(
+            segments + [0] * (max_len - len(token_ids)),
+            dtype=torch.long))
+        valid_lens.append(torch.tensor(len(token_ids), dtype=torch.float32))
+        all_pred_positions.append(torch.tensor(
+            pred_positions + [0] * (max_num_mlm_preds - len(mlm_pred_label_ids)),
+            dtype=torch.long
+        )) 
+        all_mlm_weights.append(torch.tensor(
+            [1.0] * len(pred_positions) + [0.0] * (max_num_mlm_preds - len(pred_positions)),
+            dtype=torch.float32
+        ))
+        all_mlm_labels.append(torch.tensor(
+            mlm_pred_label_ids + [0] * (max_num_mlm_preds - len(mlm_pred_label_ids)),
+            dtype=torch.long
+        ))
+        nsp_labels.append(torch.tensor(
+            is_next, dtype=torch.long
+        ))
+    
+    return (all_token_ids, all_segments, valid_lens, all_pred_positions, 
+            all_mlm_weights, all_mlm_labels, nsp_labels)
+
+
+def tokenize(lines, token='word'):
+    """Split text lines into word or character tokens"""
+    assert token in ('word', 'character'), 'Unknown token type: ' + token
+    return [line.split() if token == 'word' else list(line) for line in lines]
+
+
+class _WikiTextDataset(torch.utils.data.Dataset):
+    def __init__(self, paragraphs, max_len):
+        paragraphs = [spy.tokenize(paragraph) for paragraph in paragraphs]
+        sentences = [sentence for paragraph in paragraphs for sentence in paragraph]
+        self.vocab = spy.Vocab(sentences, min_freq=5, reserved_tokens=[
+            '<pad>', '<sep>', '<mask>', '<cls>'])
+        # Get data for the next sentence prediction task
+        examples = []
+        for paragraph in paragraphs:
+            examples.extend(_get_nsp_data_from_paragraph(
+                paragraph, paragraphs, self.vocab, max_len
+            ))
+        # Get data for the masked language modeling task
+        examples = [(_get_mlm_data_from_tokens(tokens, self.vocab) + (segments, is_next))
+                    for (tokens, segments, is_next) in examples]
+        # Pad BERT inputs
+        (self.all_token_ids, self.all_segments, self.valid_lens, self.all_pred_positions, 
+         self.all_mlm_weights, self.all_mlm_labels, self.nsp_labels) = _pad_BERT_inputs(
+             examples, max_len, self.vocab
+         )
+        
+
+    def __getitem__(self, idx):
+        return (self.all_token_ids[idx], self.all_segments[idx], self.valid_lens[idx], 
+                self.all_pred_positions[idx], self.all_mlm_weights[idx], 
+                self.all_mlm_labels[idx], self.nsp_labels[idx])
+    
+
+    def __len__(self):
+        return len(self.all_token_ids)
+     
